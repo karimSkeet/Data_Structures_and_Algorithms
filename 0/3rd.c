c4aa8aad3f3b55c63af91a3ab364802c2b4ca037
karimskeet <mohamed.karim1-etu@etu.univh2c.ma>
2021-10-14 10:08:47 +0100

    update

---
+++ 0/3rd.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	i;
+	int	j;
+	int	n;
+
+	do {
+		printf("Enter a number greater than 0\n");
+		scanf("%d", &n);
+	}while (n <= 0);
+
+	i = 0;
+	while (i < n)
+	{
+		j = 0;
+		while (j < n)
+		{
+			if (i + j + 1 > n)
+				printf("  ");
+			else
+				printf("* ");
+			j++;
+		}
+		i++;
+		printf("\n");
+	}
+	return (0);
+}
