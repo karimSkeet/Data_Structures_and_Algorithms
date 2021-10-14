17f8261f141ee57325cfea93f0c8349b75933b8c
karimskeet <mohamed.karim1-etu@etu.univh2c.ma>
2021-10-14 08:45:03 +0100

    Update

---
+++ 2nd/2nd.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+		ft_putchar(n + 48);
+}
+
+void	subarr(int *arr, int len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < len - 1)
+	{
+		j = i + 1;
+		while (j < len)
+		{
+			
