8a2f636d5c708dc2da3b2a6257fe71703b08d1b4
karimskeet <mohamed.karim1-etu@etu.univh2c.ma>
2021-10-14 10:03:00 +0100

    update

--- 0/2nd.c
+++ 0/2nd.c
@@ -11,4 +11,20 @@ int	main(void)
 		scanf("%d", &n);
 	}while (n <= 0);
 
-
+	i = 0;
+	while (i < n)
+	{
+		j = 0;
+		while (j < n)
+		{
+			if (i + j + 1 < n)
+				printf("  ");
+			else
+				printf("* ");
+			j++;
+		}
+		i++;
+		printf("\n");
+	}
+	return (0);
+}
